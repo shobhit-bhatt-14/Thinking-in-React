cf90acbe5622b7645684b8bed6dffaa6c9ee45ef
shobhit-bhatt-14 <shobhit.bhatt.14@gmail.com>
2022-04-28 15:54:55 +0530

    update

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -7,7 +7,7 @@ const FilterableProductTable = (props) => {
   const [filterText, setFilterText] = useState("");
 
   return (
-    <div className="border border-2 border-warning p-3 rounded bg-white">
+    <div className="border border-2 border-warning p-3 rounded">
       <SearchBar inStock={inStock} setInStock={setInStock} filterText={filterText} setFilterText={setFilterText} />
       <br />
       <ProductTable inStock={inStock} filterText={filterText} products={props.products} />
